df1eb9c00d45d0ce5f1ab4bf2322dd0f8c2e114f
Chloé Mahalin <chloe.mahalin@gmail.com>
2015-11-27 10:38:35 +0100

    add enum for cell states

---
+++ chloe/src/main/java/com/katas/gameoflife/CellState.java
@@ -0,0 +1,26 @@
+package com.sgcib.katas.gameoflife;
+
+/**
+ * Created by cmahalin111813 on 26/11/2015.
+ */
+public enum CellState {
+    ALIVE('*'), DEAD('.');
+
+    private char symbol;
+
+    private CellState(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public static CellState getStateFromSymbol(char symbol) {
+        if (symbol == '*') {
+            return ALIVE;
+        } else {
+            return DEAD;
+        }
+    }
+}
