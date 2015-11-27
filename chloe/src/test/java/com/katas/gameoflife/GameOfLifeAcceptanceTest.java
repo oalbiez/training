f1ea6fe196080b03c095050e45ca1d7cb35693fe
Chloé Mahalin <chloe.mahalin@gmail.com>
2015-11-27 17:53:22 +0100

    Update GameOfLifeAcceptanceTest.java

--- chloe/src/test/java/com/katas/gameoflife/GameOfLifeAcceptanceTest.java
+++ chloe/src/test/java/com/katas/gameoflife/GameOfLifeAcceptanceTest.java
@@ -8,52 +8,45 @@ import org.junit.Test;
  */
 public class GameOfLifeAcceptanceTest {
     
-   @Test
+@Test
     public void test_init_case() {
-        GameOfLife instanceToTest = new GameOfLife();
-        String result = instanceToTest.evolve(0, Universe.createUniverse(
+        String expected = "...\n" +
+                "...\n" +
+                "...";
+        
+        checkResult(expected, 1, Universe.createUniverse(
                 "...",
                 "...",
                 "..."));
-
-        String expected = "...\n"+
-                "...\n"+
-                "...";
-
-        checkResult(expected, result);
     }
 
     @Test
     public void test_cell_with_no_neighbour() {
-        GameOfLife instanceToTest = new GameOfLife();
-        String result = instanceToTest.evolve(1, Universe.createUniverse(
+        String expected = "...\n" +
+                "...\n" +
+                "...";
+
+        checkResult(expected, 1, Universe.createUniverse(
                 "*..",
                 "...",
                 "..."));
-
-        String expected = "...\n"+
-                "...\n"+
-                "...";
-
-        checkResult(expected, result);
     }
 
     @Test
     public void test_cell_with_three_neighbour() {
-        GameOfLife instanceToTest = new GameOfLife();
-        String result = instanceToTest.evolve(1, Universe.createUniverse(
+        String expected = "...\n" +
+                ".*.\n" +
+                "...";
+
+        checkResult(expected, 1, Universe.createUniverse(
                 "..*",
                 "..*",
                 "*.."));
-
-        String expected = "...\n"+
-                ".*.\n"+
-                "...";
-
-        checkResult(expected, result);
     }
 
-    public static void checkResult(String expected, String result) {
+    public static void checkResult(String expected, int numberOfSteps, Universe universe) {
+        GameOfLife instanceToTest = new GameOfLife();
+        String result = instanceToTest.evolve(numberOfSteps, universe);
         Assert.assertEquals(expected, result);
     }
 
