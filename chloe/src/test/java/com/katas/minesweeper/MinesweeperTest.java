a7bb442af4a3253ad47845deab51438c57075d80
Chloé Mahalin <chloe.mahalin@gmail.com>
2015-11-27 18:00:03 +0100

    Update MinesweeperTest.java

--- chloe/src/test/java/com/katas/minesweeper/MinesweeperTest.java
+++ chloe/src/test/java/com/katas/minesweeper/MinesweeperTest.java
@@ -8,68 +8,42 @@ import org.junit.Test;
  */
 public class MinesweeperTest {
 
-    @Test
+     @Test
     public void test_detect_Mine_no_mine() {
-        Area board = Area.generate(
-                "...",
-                "...",
-                "...");
-        Assert.assertEquals(false, board.isAMine(0, 0));
+        checkMineDetection(false, Area.generate("...", "...", "..."), 0, 0);
     }
 
     @Test
-     public void test_detect_mine_with_mine() {
-        Area board = Area.generate(
-                "*..",
-                "...",
-                "...");
-        Assert.assertEquals(true, board.isAMine(0, 0));
+    public void test_detect_mine_with_mine() {
+        checkMineDetection(true, Area.generate("*..", "...", "..."), 0, 0);
     }
 
     @Test
     public void test_detect_mine_with_mine_center() {
-        Area board = Area.generate(
-                "*..",
-                ".*.",
-                "...");
-        Assert.assertEquals(true, board.isAMine(1, 1));
+        checkMineDetection(true, Area.generate("*..", ".*.", "..."), 1, 1);
     }
 
     @Test
     public void test_detect_mine_with_mine_somewhere_else() {
-        Area board = Area.generate(
-                "*..",
-                "...",
-                ".*.");
-        Assert.assertEquals(true, board.isAMine(2, 1));
+        checkMineDetection(true, Area.generate("*..", "...", ".*."), 2, 1);
+    }
+
+    public void checkMineDetection(boolean expected, Area board, int xCoordinate, int yCoordinate) {
+        Assert.assertEquals(expected, board.isAMine(xCoordinate, yCoordinate));
     }
 
     @Test
     public void test_count_mines_around_coordinates_with_no_mines() {
-        Minesweeper game = new Minesweeper();
-        Area board = Area.generate(
-                "*..",
-                "...",
-                ".*.");
-        Assert.assertEquals(0, game.countMine(2, 1, board));
+        CheckMineCountAroundCoordinates(0, Area.generate("*..", "...", ".*."), 2, 1);
     }
 
     @Test
     public void test_count_mines_around_coordinates_with_two_mines() {
+        CheckMineCountAroundCoordinates(2, Area.generate("*..", "...", ".*."), 1, 1);
+    }
+
+    public void CheckMineCountAroundCoordinates(int expected, Area board, int xCoordinate, int yCoordinate) {
         Minesweeper game = new Minesweeper();
-        Area board = Area.generate(
-                "*..",
-                "...",
-                ".*.");
-        Assert.assertEquals(2, game.countMine(1, 1, board));
-    }
-
-
-    public static void checkResult(Area expected, Area result) {
-        Assert.assertEquals(expected, result);
-    }
-
-    public static void checkResult(String expected, String result) {
-        Assert.assertEquals(expected, result);
+        Assert.assertEquals(expected, game.countMineAroundCoordinate(xCoordinate, yCoordinate, board));
     }
 }
