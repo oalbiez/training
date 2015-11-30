c523afaeb91e14cdf9e6d1369411e5bf5d241d08
Chloé Mahalin <chloe.mahalin@gmail.com>
2015-11-30 17:02:07 +0100

    Create AreaFactory.java

---
+++ chloe/src/main/java/com/katas/minesweeper/AreaFactory.java
@@ -0,0 +1,20 @@
+package com.katas.minesweeper.Areas;
+
+/**
+ * @author Chloé Mahalin
+ */
+public class AreaFactory {
+
+    public static IArea createNewArea(String type, String... rows) {
+        if ("square".equals(type)) {
+            SquareArea area = new SquareArea(rows);
+            return area;
+        }
+        else if ("array".equals(type)) {
+            ArrayArea area = new ArrayArea(rows);
+            return area;
+        }
+        return null;
+    }
+
+}
