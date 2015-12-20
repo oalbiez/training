a9e666e63c51568c87f2b4d9e04b651bc04963b4
Chloe Mahalin <chloe.mahalin@gmail.com>
2015-12-20 19:03:26 +0100

    first step : game empty

--- chloe/tictactoetomek/src/main/java/com/katas/tictactoe/TicTacToeTomek.java
+++ chloe/tictactoetomek/src/main/java/com/katas/tictactoe/TicTacToeTomek.java
@@ -3,7 +3,7 @@ package com.katas.tictactoe;
 public class TicTacToeTomek {
 
     public String checkstate(String[] lines) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "Game has not completed";
     }
 
 }
