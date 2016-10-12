b548bfb33dcec74e71fd8011d12bf192bf79ae57
Chloé MAHALIN <chloe.mahalin@itametis.com>
2016-10-12 12:45:28 +0200

    Foobarqix

---
+++ foobarqix/src/main/java/katas/foobarqix/Foobarqix.java
@@ -0,0 +1,41 @@
+package katas.foobarqix;
+
+/**
+ *
+ * @author <a href="mailto:chloe.mahalin@itametis.com">Chloé MAHALIN - ITAMETIS</a>
+ */
+public class Foobarqix {
+
+    public static String convert(int input) {
+        StringBuilder toReturn = new StringBuilder();
+
+        if (input % 3 == 0) {
+            toReturn.append("Foo");
+        }
+        if (input % 5 == 0) {
+            toReturn.append("Bar");
+        }
+        if (input % 7 == 0) {
+            toReturn.append("Qix");
+        }
+
+        for (char caracter : Integer.toString(input).toCharArray()) {
+            switch (caracter) {
+                case '3':
+                    toReturn.append("Foo");
+                    break;
+                case '5':
+                    toReturn.append("Bar");
+                    break;
+                case '7':
+                    toReturn.append("Qix");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return toReturn.toString().isEmpty() ? Integer.toString(input) : toReturn.toString();
+    }
+
+}
---
+++ foobarqix/src/test/java/katas/foobarqix/FoobarqixTest.java
@@ -0,0 +1,82 @@
+package katas.foobarqix;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author <a href="mailto:chloe.mahalin@itametis.com">Chloé MAHALIN - ITAMETIS</a>
+ */
+public class FoobarqixTest {
+
+    @Test
+    public void number_multiple_of_three_contain_3() {
+        this.compare("FooFoo", 3);
+    }
+
+
+    @Test
+    public void confirm_number_multiple_of_three_write_foo_instead_of_number() {
+        this.compare("Foo", 6);
+    }
+
+
+    @Test
+    public void number_multiple_of_five_contain_5() {
+        this.compare("BarBar", 5);
+    }
+
+
+    @Test
+    public void confirm_number_multiple_of_five_write_bar_instead_of_number() {
+        this.compare("Bar", 10);
+    }
+
+
+    @Test
+    public void number_multiple_of_seven_contain_seven() {
+        this.compare("QixQix", 7);
+    }
+
+
+    @Test
+    public void confirm_number_multiple_of_seven_write_bar_instead_of_number() {
+        this.compare("Qix", 14);
+    }
+
+
+    @Test
+    public void random_number_not_multiple_3_5_7() {
+        this.compare("8", 8);
+    }
+
+
+    @Test
+    public void multiple_3_5_contains_5() {
+        this.compare("FooBarBar", 15);
+    }
+
+
+    @Test
+    public void multiple_3_contains_3_3() {
+        this.compare("FooFooFoo", 33);
+    }
+
+
+    @Test
+    public void multiple_3_7() {
+        this.compare("FooQix", 21);
+    }
+
+
+    @Test
+    public void contain_5_3() {
+        this.compare("BarFoo", 53);
+    }
+
+
+    public void compare(String expected, int input) {
+        Assert.assertEquals(expected, Foobarqix.convert(input));
+    }
+
+}
