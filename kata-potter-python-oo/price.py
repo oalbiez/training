daf86828c65581f8251f3feca0042193d9c6b96c
Olivier Albiez <olivier.albiez@poloper.org>
2017-09-04 21:26:05 +0200

    Update kata-potter

--- kata-potter-python-oo/price.py
+++ kata-potter-python-oo/price.py
@@ -14,7 +14,7 @@ class PercentDiscount(object):
         self.__value = value
 
     def compute(self, price):
-        return Price(price.value * (1.0 - self.__value))
+        return Price(price.amount * (1.0 - self.__value))
 
     def __repr__(self):
         return "PercentDiscount(" + str(self.__value) + ")"
@@ -23,41 +23,41 @@ class PercentDiscount(object):
 class Price(object):
 
     @property
-    def value(self):
-        return self.__value
+    def amount(self):
+        return self.__amount
 
-    def __init__(self, value):
-        self.__value = value
+    def __init__(self, amount):
+        self.__amount = amount
 
     def discount(self, discount):
         return discount.compute(self)
 
     def __eq__(self, other):
-        return self.__value == other.value
+        return self.__amount == other.amount
 
     def __ne__(self, other):
-        return self.__value != other.value
+        return self.__amount != other.amount
 
     def __lt__(self, other):
-        return self.__value < other.value
+        return self.__amount < other.amount
 
     def __le__(self, other):
-        return self.__value <= other.value
+        return self.__amount <= other.amount
 
     def __gt__(self, other):
-        return self.__value > other.value
+        return self.__amount > other.amount
 
     def __ge__(self, other):
-        return self.__value >= other.value
+        return self.__amount >= other.amount
 
     def __add__(self, other):
-        return Price(self.__value + other.value)
+        return Price(self.__amount + other.amount)
 
     def __mul__(self, factor):
-        return Price(self.__value * factor)
+        return Price(self.__amount * factor)
 
     def __sub__(self, other):
-        return Price(self.__value - other.value)
+        return Price(self.__amount - other.amount)
 
     def __repr__(self):
-        return "Price(" + str(self.__value) + ")"
+        return "Price(" + str(self.__amount) + ")"
