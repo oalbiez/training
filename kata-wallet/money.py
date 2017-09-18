a389c0040ee9e1344e72c755a88fb7d1b47ec441
Olivier Albiez <olivier.albiez@poloper.org>
2017-09-18 22:26:14 +0200

    Refactor money

--- kata-wallet/money.py
+++ kata-wallet/money.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from functools import total_ordering
 
 
 class Currency(namedtuple('Currency', 'symbol code precision')):
@@ -6,8 +7,11 @@ class Currency(namedtuple('Currency', 'symbol code precision')):
     def round_amount(self, amount):
         return round(amount, self.precision)
 
+    def render(self, amount):
+        return self.code + " " + str(amount)
+
     def __call__(self, amount):
-        return Money(amount, self)
+        return Amount(self.round_amount(amount), self)
 
 
 EUR = Currency("€", "EUR", 2)
@@ -15,7 +19,8 @@ USD = Currency("$", "USD", 3)
 XBT = Currency("BTC", "XBT", 8)
 
 
-class Money(object):
+@total_ordering
+class Amount(object):
 
     @property
     def amount(self):
@@ -26,7 +31,7 @@ class Money(object):
         return self.__currency
 
     def __init__(self, amount, currency):
-        self.__amount = currency.round_amount(amount)
+        self.__amount = amount
         self.__currency = currency
 
     def is_currency(self, currency):
@@ -35,15 +40,18 @@ class Money(object):
     def change(self, new_currency, exchange_rates):
         if self.is_currency(new_currency):
             return self
-        return Money(self.__amount * exchange_rates(self.__currency, new_currency), new_currency)
+        return new_currency(self.__amount * exchange_rates(self.__currency, new_currency))
 
     def __eq__(self, other):
         return self.__amount == other.amount and self.__currency == other.currency
 
+    def __lt__(self, other):
+        return self.__amount < other.amount and self.__currency == other.currency
+
     def __add__(self, other):
         if self.is_currency(other.currency):
-            return Money(self.__amount + other.amount, self.__currency)
+            return self.__currency(self.__amount + other.amount)
         raise TypeError("Mismatch currency, expected " + self.__currency.code + " got " + other.currency.code)
 
     def __repr__(self):
-        return self.__currency.code + " " + str(self.__amount)
+        return self.__currency.render(self.__amount)
