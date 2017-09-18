c631965507de24f9e5b7d47535bc7ebe1ed4b8ad
Olivier Albiez <olivier.albiez@poloper.org>
2017-09-18 22:35:21 +0200

    Cleanup unused feature

--- kata-wallet/currency.py
+++ kata-wallet/currency.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 from functools import total_ordering
 
 
-class Currency(namedtuple('Currency', 'symbol code precision')):
+class Currency(namedtuple('Currency', 'code precision')):
 
     def round_amount(self, amount):
         return round(amount, self.precision)
@@ -14,24 +14,24 @@ class Currency(namedtuple('Currency', 'symbol code precision')):
         return Amount(self.round_amount(amount), self)
 
 
-EUR = Currency("€", "EUR", 2)
-USD = Currency("$", "USD", 3)
-XBT = Currency("BTC", "XBT", 8)
+EUR = Currency("EUR", 2)
+USD = Currency("USD", 3)
+XBT = Currency("XBT", 8)
 
 
 @total_ordering
 class Amount(object):
 
     @property
-    def amount(self):
-        return self.__amount
+    def value(self):
+        return self.__value
 
     @property
     def currency(self):
         return self.__currency
 
-    def __init__(self, amount, currency):
-        self.__amount = amount
+    def __init__(self, value, currency):
+        self.__value = value
         self.__currency = currency
 
     def is_currency(self, currency):
@@ -40,18 +40,18 @@ class Amount(object):
     def change(self, new_currency, exchange_rates):
         if self.is_currency(new_currency):
             return self
-        return new_currency(self.__amount * exchange_rates(self.__currency, new_currency))
+        return new_currency(self.__value * exchange_rates(self.__currency, new_currency))
 
     def __eq__(self, other):
-        return self.__amount == other.amount and self.__currency == other.currency
+        return self.__value == other.value and self.__currency == other.currency
 
     def __lt__(self, other):
-        return self.__amount < other.amount and self.__currency == other.currency
+        return self.__value < other.value and self.__currency == other.currency
 
     def __add__(self, other):
         if self.is_currency(other.currency):
-            return self.__currency(self.__amount + other.amount)
+            return self.__currency(self.__value + other.value)
         raise TypeError("Mismatch currency, expected " + self.__currency.code + " got " + other.currency.code)
 
     def __repr__(self):
-        return self.__currency.render(self.__amount)
+        return self.__currency.render(self.__value)
