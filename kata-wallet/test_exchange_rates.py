8451c9a905e7404d9afcabc59455e052b6006659
Olivier Albiez <olivier.albiez@poloper.org>
2017-09-19 19:29:49 +0200

    Create two tests suites: normal test and slow test

--- kata-wallet/test_exchange_rates.py
+++ kata-wallet/test_exchange_rates.py
@@ -1,15 +1,19 @@
+from pytest import mark
 from exchange_rates import google_exchange_rates, yahoo_exchange_rates
 from wallet import Wallet
 from currency import USD, EUR
 
 
-def _test_yahoo_exchange_rates_should_be_not_null():
+@mark.slowtest
+def test_yahoo_exchange_rates_should_be_not_null():
     assert yahoo_exchange_rates()(EUR, USD) > 0
 
 
-def _test_google_exchange_rates_should_be_not_null():
+@mark.slowtest
+def test_google_exchange_rates_should_be_not_null():
     assert google_exchange_rates()(EUR, USD) > 0
 
 
-def _test_real_conversion():
+@mark.slowtest
+def test_real_conversion():
     assert Wallet(EUR(10), USD(0.01)).amount(EUR, google_exchange_rates()) > EUR(10)
