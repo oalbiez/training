4e3d7b2cfdce8d2aed894ddc9f6f9b6018f6c071
Olivier Albiez <olivier.albiez@poloper.org>
2017-09-20 16:01:04 +0200

    Refactor properties in test

--- kata-wallet/test_wallet.py
+++ kata-wallet/test_wallet.py
@@ -1,16 +1,19 @@
 from hypothesis import given, assume
-from hypothesis.strategies import integers, lists
+from hypothesis.strategies import integers, lists, builds
 
 from wallet import Wallet
 from currency import USD, EUR, DZD, KRW, XBT
 from exchange_rates import fixed_exchange_rates
 
 
+Euros = integers(min_value=1, max_value=100000).map(EUR)
+
+
 def test_empty_wallet_should_have_amount_of_0():
     assert amount_in_euro(Wallet()) == EUR(0)
 
 
-@given(lists(integers(min_value=1, max_value=100000).map(EUR)))
+@given(lists(Euros))
 def test_wallet_should_sum_items_with_same_currency(amounts):
     assume(len(amounts) > 1)
     assert amount_in_euro(Wallet(*amounts)) == sum(amounts, EUR(0))
@@ -20,7 +23,7 @@ def test_wallet_should_change_item_to_required_currency():
     assert amount_in_euro(Wallet(USD(10)), fixed_exchange_rates("1 USD = 1.1 EUR")) == EUR(11)
 
 
-def test_real_conversion():
+def test_conversion_of_multiple_currencies():
     assert amount_in_euro(
         Wallet(EUR(10), DZD(50), KRW(200), XBT(0.00002354)),
         fixed_exchange_rates("1 DZD = 0.01 EUR", "1 KRW = 0.001 EUR", "1 XBT = 4000 EUR")) == EUR(10.79)
